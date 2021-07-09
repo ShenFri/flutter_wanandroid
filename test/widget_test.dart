8198233a01fcdad32ef3d8aa5040672404a0b103
ShenFei <444354821@qq.com>
2021-07-09 10:37:12 +0800

    del baseBean

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_wanandroid/main.dart';
+import 'package:flutter_agent_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
